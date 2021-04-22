de3ef02e6fea2cc2dce0a0a2b8f891c18d7d3f0c
vinicius4g <viniciusdias1701@hotmail.com>
2021-04-22 18:17:32 -0300

    aula04

---
+++ src/http.ts
@@ -0,0 +1,38 @@
+import express, { request, response } from 'express';
+import { createServer } from "http"
+import { Server, Socket } from "socket.io"
+import path from "path"
+
+import "./database"
+import { routes } from './routes'
+
+const app = express()
+
+//todas as configuracoes para poder utilizar o html dentro das rotas
+app.use(express.static(path.join(__dirname, "..", "public"))) //fazendo a aplicao enxergar o front (public)
+app.set("views", path.join(__dirname, "..", "public")) //setando onde a views estarao
+app.engine("html", require("ejs").renderFile) //informar qual a engine que vai ser utilizada
+app.set("view engine", "html")
+
+
+app.get("/pages/client",(request, response) => {
+    return response.render("html/client.html")
+})
+
+//criando um servido http e utilizando tudo que tem dentro do app
+const http = createServer(app) //criando o protocolo http
+
+//criando um servidor do socket.io, passando o protocolo http
+const io = new Server(http) //criando o protocolo ws
+
+//parte para o usuario se conectar com o web socket
+io.on("connection", (socket: Socket) => {
+    // console.log("Se conectou", socket.id)
+})
+
+//definindo que podem chegar req em formato json
+app.use(express.json())
+
+app.use(routes)
+
+export { http, io } 
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,5 @@
-import express from 'express';
-
-import "./database"
-import { routes } from './routes'
-
-const app = express();
-
-//definindo que podem chegar req em formato json
-app.use(express.json())
-
-app.use(routes)
+import { http } from "./http"
+import "./websocket/client"
 
 //iniciando a aplicacao
-app.listen(3333, () => console.log('Backend rodando, na porta 3333'))
+http.listen(3333, () => console.log('Backend rodando, na porta 3333'))
